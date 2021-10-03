02915ce7ae41e61e97d3a587cd8bc02b96f0e076
jsmineau <jasmineaudreey@gmail.com>
2021-10-03 22:27:36 +0700

    components pop up

---
+++ components/AboutUs.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="AboutUs">About Us</div>
+        <!-- ini bisa buat components lagi, bisa juga buat contentnya yaa -->
+    </div>
+</template>
+
+<script>
+    export default {
+        // ini buat naro script script yg diperluin buat websitenya, intinya logic nya inituh.
+    }
+</script>
+
+<style  scoped>
+.AboutUs{
+    background-color: #466584;
+    border-radius: 25px;
+    position: absolute;
+    width: 630px;
+    height: 400px;
+    left: 960px;
+    top: 400px;
+    transform: translate(-50%, -50%);
+    color: #E7E8E7;
+
+    font-family: 'Tf Grotesk';
+    font-style: italic;
+    font-weight: normal;
+    font-size: 48px;
+    line-height: 150px;
+    text-align: left;
+    padding-left: 70px;
+}
+    /* ini buat stylingnya, ngasih warna, benerin posisi, benerin ukuran, font, kasih border dll. */
+</style>
