590c9432c3a37259772cd24ce57dd671ea9fe863
Hambinn <ilhambintangn@gmail.com>
2021-10-05 12:29:06 +0700

    nambahin database

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,26 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config:{
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        },
+        services:{
+          firestore: true,
+          storage: true,
+          database: true,
+          analytics: true
+        }
+      }
+    ]
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
