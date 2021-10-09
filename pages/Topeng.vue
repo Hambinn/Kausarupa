03013bcb2006dc0b3c43022004e399869f016e94
srhfdl <sarahfadhila22@gmail.com>
2021-10-09 21:24:00 +0700

    commit on 'main'

--- pages/Topeng.vue
+++ pages/Topeng.vue
@@ -14,7 +14,7 @@
 
 .bg-tema{
     text-align: center;
-    background-image: url("D:/Teknis GEP/Kausarupa/assets/svg/ChoosePage/background (3).svg");
+    background-image: url("~/assets/svg/ChoosePage/background (3).svg");
     background-repeat: no-repeat;
     background-size: 100% 100%;
     height: 100vh;
