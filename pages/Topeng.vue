1dce3d79b37cea084427bfb1428de74105036e0a
srhfdl <sarahfadhila22@gmail.com>
2021-10-09 11:23:41 +0700

    commit on 'main'

--- pages/Topeng.vue
+++ pages/Topeng.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="bg-tema">
         <p>hai sarah</p>
     </div>
 </template>
@@ -10,6 +10,15 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style>
+
+.bg-tema{
+    text-align: center;
+    background-image: url("D:/Teknis GEP/Kausarupa/assets/svg/ChoosePage/background (3).svg");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 100vh;
+    width: 100vw;
+    }
 
 </style>
