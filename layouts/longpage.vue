97dc2a1984d01a0b48b27d2a4e79bae22b78ae5a
Hambinn <ilhambintangn@gmail.com>
2021-10-20 22:16:37 +0700

    nambahin layout baru untuk main.vue

---
+++ layouts/longpage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg">
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    body, html{
+        height: 100%;
+        margin: 0;
+    }
+
+    body{
+        background-image: url("../assets/png/MainPage/bg_gelap.png");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100%;
+    }
+
+</style>
