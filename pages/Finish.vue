4651eac8531a53ebb0c079142b12de44dc39495b
jsmineau <jasmineaudreey@gmail.com>
2021-10-30 04:22:39 +0700

    tambah page inish

---
+++ pages/Finish.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container-finish">
+        <div class="top-cont-finish">
+            <div class="canvas-finish">
+                <div class="lemari-finish">
+                </div>
+                <div class="meja-finish">
+                    <img src="~/assets/png/FinishPage/Last/meja (1)-min.png" alt="" class="meja-finish">
+                </div>
+                <div class="bunga-finish">
+                    <img src="~/assets/png/FinishPage/Last/vas bunga-min.png" alt="" class="bunga-finish">
+                </div>
+                <div class="orang-finish">
+                    <img src="~/assets/png/FinishPage/Last/orang-min" alt="" class="orang-finish">
+                </div>
+                <div class="jendela-finish">
+                    <img src="~/assets/png/FinishPage/Last/jendela (1)-min.png" alt="" class="jendela-finish">
+                </div>
+                <div class="topeng-finish">
+                    <img src="~/assets/png/FinishPage/Last/3 topeng.png" alt="" class="topeng-finish">
+                </div>
+                <div class="logo-finish">
+                    <img src="~/assets/png/FinishPage/Last/logo kausarupa.png" alt="" class="logo-finish">
+                </div>
+                <div class="cont-tombol-finish">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+html.body{
+    all: unset
+}
+
+.top-cont-finish{
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.container-finish{
+    position: absolute;
+    text-align: center;
+    background-image: url("../assets/png/FinishPage/bg umum last page.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    }
+
+</style>
