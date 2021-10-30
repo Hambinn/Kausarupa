fca4fc7b6f6407dd0e39a59d73ba9a602ff6a1e9
kikirifqi7 <rifqiadiguna@gmail.com>
2021-10-30 10:29:02 +0700

    Finish Page

--- pages/Finish.vue
+++ pages/Finish.vue
@@ -1,32 +1,27 @@
 <template>
     <div class="container-finish">
-        <div class="top-cont-finish">
-            <div class="canvas-finish">
-                <div class="lemari-finish">
-                    <img src="../assets/png/FinishPage/Last/lemari (1)-min.png" alt="">
-                </div>
-                <div class="meja-finish">
-                    <img src="../assets/png/FinishPage/Last/meja (1)-min.png" alt="">
-                </div>
-                <div class="bunga-finish">
-                    <img src="../assets/png/FinishPage/Last/vas bunga-min.png" alt="">
-                </div>
-                <div class="orang-finish">
-                    <img src="../assets/png/FinishPage/Last/orang-min.png" alt="">
-                </div>
-                <div class="jendela-finish">
-                    <img src="../assets/png/FinishPage/Last/jendela (1)-min.png" alt="">
-                </div>
-                <div class="topeng-finish">
-                    <img src="../assets/png/FinishPage/Last/3 topeng.png" alt="">
-                </div>
-                <div class="logo-finish">
-                    <img src="../assets/png/FinishPage/Last/logo kausarupa.png" alt="">
-                </div>
-                <div class="cont-tombol-finish">
-                </div>
-            </div>
+      <div class="top-cont-finish">
+        <div class="canvas-finish">
+          <div class="container-orang">
+            <img src="../assets/svg/LastPage/orang-min.png" alt="" class="OrangLast">
+          </div>
+          <div class="container-meja-last">
+            <img src="../assets/svg/LastPage/meja (1)-min.png" alt="" class="MejaLast">
+          </div>
+          <div class="container-vas-last">
+            <img src="../assets/svg/LastPage/vas bunga-min.png" alt="" class="VasLast">
+          </div>
+          <div class="container-topeng-last">
+            <img src="../assets/svg/LastPage/3 topeng.png" alt="" class="TopengLast">
+          </div>
+          <div class="container-lemari-last">
+            <img src="../assets/svg/LastPage/lemari (1)-min.png" alt="" class="LemariLast">
+          </div>
+          <div class="container-logo-last">
+            <img src="../assets/svg/LastPage/logo kausarupa.png" alt="" class="LogoLast">
+          </div>
         </div>
+      </div>
     </div>
 </template>
 
@@ -37,6 +32,12 @@
 
 html.body{
     all: unset
+}
+
+*{
+        padding: 0;
+        margin: 0;
+        
 }
 
 .top-cont-finish{
@@ -58,9 +59,85 @@ html.body{
     overflow: hidden;
     }
 
-.lemari-finish{
+.container-orang{
     position: absolute;
-    height: 40%;
-    transform: translate(20%, 90%);
+    width: 100%;
+    height: 100%;
+}
+
+.container-orang .OrangLast{
+    position: absolute;
+    height: 65%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-52%);
+}
+
+.container-meja-last{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.container-meja-last .MejaLast{
+    position: absolute;
+    height: 10%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-100%,150%);
+}
+
+.container-vas-last{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.container-vas-last .VasLast{
+    position: absolute;
+    height: 65%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-52%);
+}
+
+.container-topeng-last{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.container-topeng-last .TopengLast{
+    position: absolute;
+    height: 30%;
+    top: 50%;
+    left: 50%;
+    transform: translate(0%,0%);
+}
+
+.container-lemari-last{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.container-lemari-last .LemariLast{
+    height: 30%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-52%);
+}
+
+.container-logo-last{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.container-logo-last .LogoLast{
+    height: 30%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-52%);
 }
 </style>
