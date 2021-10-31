ed6b3e8240a42f6c6ae1309fa8e3cde248cf37dd
jsmineau <jasmineaudreey@gmail.com>
2021-10-31 23:40:15 +0700

    tambah components finish

---
+++ components/TombolFinish.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>  
+        <div class="katalog-finish">
+            <img src="../assets/png/FinishPage/Last/katalog.png">
+            <p> Katalog </p>
+        </div>
+
+        <div class="merch-finish">
+            <img src="../assets/png/FinishPage/Last/merch.png">
+            <p> Merch </p> 
+        </div>
+
+        <div class="photobooth-finish">
+            <img src="../assets/png/FinishPage/Last/photobooth.png">
+            <p> Photobooth </p>
+        </div>
+
+        <div class="gepsebelumnya-finish">
+            <img src="../assets/png/FinishPage/Last/gep sebelumnya.png">
+            <p> GEP Sebelumnya </p>
+        </div>
+        <div class="homebutton-finish">
+            <img src="../assets/png/FinishPage/home.png">
+        </div>
+        <div class="feedback-finish">
+            <p> Feedback </p>
+        </div>
+  </div>  
+</template>
+
+<script>
+</script>
+
+<style>
+
+.katalog-finish{
+    position: absolute;
+    height: 10%;
+    width: 10%;
+    transform: translate(70%, 80%);
+
+    font-family: Tf Grotesk;
+    font-style: italic;
+    font-weight: normal;
+    font: size 10vh;
+    color: #466584;
+}
+
+.merch-finish{
+    position: absolute;
+    height: 10%;
+    width: 10%;
+    transform: translate(120%, 70%);
+
+    font-family: Tf Grotesk;
+    font-style: italic;
+    font-weight: normal;
+    font: size 10vh;
+    color: #466584;
+}
+
+.photobooth-finish{
+    position: absolute;
+    height: 10%;
+    width: 10%;
+    transform: translate(70%, 300%);
+
+    font-family: Tf Grotesk;
+    font-style: italic;
+    font-weight: normal;
+    font: size 10vh;
+    color: #466584;
+}
+
+.gepsebelumnya-finish{
+    position: absolute;
+    height: 10%;
+    width: 10%;
+    transform: translate(120%,300%);
+
+    font-family: Tf Grotesk;
+    font-style: italic;
+    font-weight: normal;
+    font: size 10vh;
+    color: #466584;
+}
+
+.homebutton-finish{
+    position: absolute;
+    height: 5%;
+    width: 5%;
+    transform: translate(10%, 80%);
+}
+
+.feedback-finish{
+    position: absolute;
+    height: 50%;
+    width: 70%;
+    transform: translate(0%, 0%);
+    color:#466584;
+
+    font-family: Tf Grotesk;
+    font-style: italic;
+    font-weight: normal;
+    font: size 10vh;
+    color: #ffffff;
+}
+</style>
