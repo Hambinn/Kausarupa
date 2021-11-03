a3f21370233a5272af2b811042c49b0e8da0f1f9
fayzanadia@gmail.com <fayzanadia@gmail.com>
2021-11-03 16:45:35 +0700

    add page credits panitia buku tamu

---
+++ pages/BukuTamu.vue
@@ -0,0 +1,208 @@
+<template>
+    <div class="bg-buku-tamu">
+        <div class="top-cont">
+            <div class="canvas">
+                <div class="container-text">
+                    <div class="isi-head">
+                        <p class="text-heading">Muhammad Nadhif Adristia</p>
+                        <p class="text-subheading">Ketua Umum LFM ITB 2021/2022</p>
+                        <br>
+                        <p class="text-heading">Muhammad Faizin Fitriansyah</p>
+                        <p class="text-subheading">Ketua Bidang Pertunjukan 2021/2022</p>
+                        <br>
+                        <p class="text-heading">Husnul Abidah</p>
+                        <p class="text-subheading">Pimpinan Proyek GEP: Kausarupa 2021</p>
+                    </div>
+                    <div class="container-isi-kiri">
+                        <div class="isi-acara">
+                            <p class="text-heading">Acara</p>
+                            <p class="text-subheading">Joshua Theodore Setyawan</p>
+                            <p class="text-subheading">Iffah Zati Mazaya</p>
+                            <p class="text-subheading">Anggi Afifah Harahap</p>
+                            <p class="text-subheading">Farhan Zaki Rahmani</p>
+                            <p class="text-subheading">Athallah Farrelitavio</p>
+                            <p class="text-subheading">Jeremy Christian Tandiary</p>
+                            <p class="text-subheading">Nisa Aulia Rahmani</p>
+                            <p class="text-subheading">Akhmad Falah Herjan</p>
+                            <p class="text-subheading">Muhammad Hassan Rafi Akbari</p>
+                            <p class="text-subheading">Faura Achtamirella Rudanty</p>
+                            <p class="text-subheading">Kamal</p>
+                            <br>
+                        </div>
+                        <div class="isi-danus">
+                            <p class="text-heading">Dana Usaha</p>
+                            <p class="text-subheading">Azzah Naurah Rusman</p>
+                            <p class="text-subheading">Dinan Firdaus</p>
+                            <p class="text-subheading">Zikrina Firdausyah Machfud</p>
+                            <p class="text-subheading">Zakiyah Artanti Ratnadewi</p>
+                            <p class="text-subheading">Hamzah Awnez Falihi</p>
+                            <p class="text-subheading">Alya Nissa Haliza</p>
+                            <p class="text-subheading">Dimas Putra Wahyudi</p>
+                            <p class="text-subheading">Kawar Vincensius E Brahmana</p>
+                            <br>
+                        </div>
+                        <div class="isi-kreatif">
+                            <p class="text-heading">Kreatif</p>
+                            <p class="text-subheading">Andrea Cattleya</p>
+                            <p class="text-subheading">Faiha Meiry Salsabiila</p>
+                            <p class="text-subheading">Asysyifa Asri Adityarini</p>
+                            <p class="text-subheading">Bintan Haya</p>
+                            <p class="text-subheading">Faiza Kamilah</p>
+                            <p class="text-subheading">Vania Amara</p>
+                            <p class="text-subheading">Rizqa Miranda</p>
+                            <p class="text-subheading">Aldi Fadlian Sunan</p>
+                            <p class="text-subheading">Gracelynn Joanne</p>
+                            <p class="text-subheading">Maria Marcella Faustine</p>
+                            <br>
+                        </div>
+                        <div class="isi-teknis">
+                            <p class="text-heading">Teknis</p>
+                            <p class="text-subheading">Ilham Bintang Nurmansyah</p>
+                            <p class="text-subheading">Claudia</p>
+                            <p class="text-subheading">Fayza Nadia</p>
+                            <p class="text-subheading">Faza Adzim Ramadhon</p>
+                            <p class="text-subheading">Fiona Angelica Tjahjono</p>
+                            <p class="text-subheading">Muhammad Rifqi Adiguna</p>
+                            <p class="text-subheading">Imala Askania Faza</p>
+                            <p class="text-subheading">Sarah Fadhila</p>
+                            <p class="text-subheading">Jasmine Audrey Kurniawan</p>
+                            <p class="text-subheading">Natasha Tiovanny Silaban</p>
+                        </div>
+                        <div class="container-isi-kanan">
+                            <div class="isi-dokum">
+                                <p class="text-heading">Publikasi dan Dokumentasi</p>
+                                <p class="text-subheading">Dinda Nada Alifah</p>
+                                <p class="text-subheading">Vincentius Adrian Laurens</p>
+                                <p class="text-subheading">Sabrina Prajnalita</p>
+                                <p class="text-subheading">Khanaya Saifana Ridwan</p>
+                                <p class="text-subheading">Amira Kylanindita</p>
+                                <p class="text-subheading">Defani Athira Andini</p>
+                                <p class="text-subheading">Zavira Hana Salsabila</p>
+                                <p class="text-subheading">Jeanne Belle Leo Fiona</p>
+                                <p class="text-subheading">Rafif Janitra Aryadito</p>
+                                <p class="text-subheading">Nasywa Rahma Fatiha</p>
+                                <p class="text-subheading">Muhammad Irfan Fauzandi Putra</p>
+                                <p class="text-subheading">Thobie Devara Prayusdiandra</p>
+                                <p class="text-subheading">Nindya Yunita Hanifa</p>
+                                <p class="text-subheading">Rifqy Armansyah</p>
+                                <p class="text-subheading">Joshua Tiroy Lumbantobing</p>
+                                <br>
+                            </div>
+                            <div class="isi-kurasi">
+                                <p class="text-heading">Kurasi</p>
+                                <p class="text-subheading">Kevin Harsana</p>
+                                <p class="text-subheading">Robin</p>
+                                <p class="text-subheading">Tsamania Salsabila</p>
+                                <p class="text-subheading">Muhammad Andisha Farra-Izadi</p>
+                                <p class="text-subheading">Antonius Hanindya Surya Triyoga</p>
+                                <p class="text-subheading">Nuraliya Dewanti</p>
+                                <p class="text-subheading">Rosemary Emmanuella Dame S</p>
+                                <p class="text-subheading">Meidina Puteri Aginsa</p>
+                                <p class="text-subheading">Istifa Yurbi</p>
+                                <p class="text-subheading">Aditias Alif Mardiano</p>
+                                <p class="text-subheading">Nadissa Regina Putri</p>
+                                <p class="text-subheading">Latisha Kyravashti Erfifani</p>
+                                <p class="text-subheading">Aditya Mulya Nugraha</p>
+                                <p class="text-subheading">Hemas Farhana Larasati</p>
+                                <p class="text-subheading">Nabila Suci Annisa</p>
+                                <p class="text-subheading">Namira Mezzaluna Anjani</p>
+                                <p class="text-subheading">Samuel Sautma Tulus Sianipar</p>
+                                <p class="text-subheading">Samuel Obadiah</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+html,body{
+    margin-top: -1%;
+}
+
+body, html{
+    height: 100%;
+    width: 100%;
+    overflow-x: hidden;
+    }
+
+body{
+    background-image: url("~/assets/png/BukuTamuPage/bg panjang 16_9.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100%;
+    overflow: hidden;
+}
+
+.top-cont{
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding-bottom: 56.25%;
+}
+
+.canvas{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.container-text{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+
+.container-text .isi-head{
+    top: 50%;
+    left: 50%;
+    transform: translate(0%, 90%);
+}
+
+.container-text .container-isi-kiri{
+    top: 50%;
+    left: 50%;
+    transform: translate(-25%, 15%);
+}
+
+.container-text .container-isi-kanan{
+    top: 50%;
+    left: 50%;
+    transform: translate(50%, -132.5%);
+}
+
+.text-heading{
+    font-family: 'Tf Grotesk-italic';
+    font-style: italic;
+    font-weight: normal;
+    font-size: 1.6vw;
+    line-height: 20%;
+    text-align: center;
+
+    color: #000000;
+}
+
+.text-subheading{
+    font-family: 'Gaegu', cursive;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.6vw;
+    line-height: 20%;
+    text-align: center;
+
+    color: #000000;
+}
+
+</style>
