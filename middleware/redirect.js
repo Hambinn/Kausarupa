0e584b2c0c78e6d2d536a016bad82be8c7ac6cea
Hambinn <ilhambintangn@gmail.com>
2021-11-08 01:14:38 +0700

    cd

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,4 +1,4 @@
-const gepDate = new Date("Nov 8, 2021 00:00:00").getTime();
+const gepDate = new Date("Nov 8, 2021 20:45:00").getTime();
 export default function(ctx){
     const now = new Date().getTime();
     if(gepDate - now > 0){
